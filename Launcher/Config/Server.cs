72bedafca2ee03e6d82c6e8e5ec9dcaaecc7a8f1
GeekOfWires <wmbowles201@gmail.com>
2018-10-21 22:00:24 -0400

    Update to Launcher

---
+++ Launcher/Config/Server.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Launcher.Config
+{
+    [JsonObject]
+    public class Server
+    {
+        [JsonRequired]
+        [JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+        [JsonRequired]
+        [JsonProperty(PropertyName = "Address")]
+        public IPAddress Address { get; set; }
+    }
+}
---
+++ Launcher/GameCheck/File.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Launcher.GameCheck
+{
+    public class File
+    {
+        private readonly string _fileHash;
+
+        public File(string filename)
+        {
+            FileName = filename;
+
+            _fileHash = GetMD5Hash();
+        }
+
+        private string GetMD5Hash()
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var fileStream = System.IO.File.OpenRead(FileName))
+                {
+                    var md5hash = md5.ComputeHash(fileStream);
+
+                    return Encoding.Default.GetString(md5hash);
+                }
+            }
+        }
+
+        public string FileHash => _fileHash;
+        public string FileName { get; set; }
+    }
+}
