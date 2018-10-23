fe7afefdb3d9243972a47db675d2cf79f2864c2c
GeekOfWires <wmbowles201@gmail.com>
2018-10-23 02:51:25 -0400

    This rascal is up now.

---
+++ PSForeverLauncher/Framework/Messages/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace PSForeverLauncher.Framework.Messages
+{
+    public interface IMessage
+    {
+
+    }
+
+    public class Message : IMessage
+    {
+
+    }
+}
---
+++ PSForeverLauncher/ViewModels/ServerListViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PSForeverLauncher.Framework.Messages;
+
+namespace PSForeverLauncher.ViewModels
+{
+    public interface IServerListViewModel : IScreen
+    {
+        void Return();
+    }
+
+    public class ServerListViewModel : Screen, IServerListViewModel
+    {
+        public IEventAggregator EventAggregator { get; set; }
+
+        public ServerListViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+        }
+
+
+        public void Return()
+        {
+            _eventAggregator.PublishOnUIThread(new OpenLaunchViewMessage());
+        }
+
+        private readonly IEventAggregator _eventAggregator;
+    }
+}
